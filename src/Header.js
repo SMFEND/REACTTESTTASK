a5365ca372884700071ce7c8ae8a26ff15d97cf5
unknown <smernyaev@gmail.com>
2021-07-30 23:43:43 +0400

    final

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,10 @@ function Header() {
     return (
         <header>
             <h2>I'm header</h2>
+            <a href="#"><h2>Home</h2></a>
+            <a href="#"><h2>Main</h2></a>
+            <a href="#"><h2>About</h2></a>
+            <a href="#"><h2>Exit</h2></a>
         </header>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,9 @@
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <h1>Click any position in the table to make it displayed in the footer element</h1>
+            <h1>Click any <span style={{textShadow: '0px 0px 1px #FFF', textDecoration:'underline solid white'}}>th</span> element to sort the table out</h1>
+            <h1>Feel free to search through the table with the input</h1>
         </div>
     )
 }
